241abf6b64c07c315fd2297cbbbb0f497e11171b
yarocruz <yarocruz@gmail.com>
2021-03-12 19:35:12 -0500

    refactoring functionality

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,28 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     // then we index into the vector and grab those strings
-    let query = &args[1]; // this first one will be the pattern we search for
-    let filename = &args[2]; // this one will be the source file we're searching into
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
     // get the contents of the file
-    let contents = fs::read_to_string(filename) // read_to_string returns a Result type
+    let contents = fs::read_to_string(config.filename) // read_to_string returns a Result type
         .expect("Something went wrong reading the file"); // and that's why we can add the .expect on it
 
     println!("With text:\n{}", contents);
+}
+
+// making a Struct here to convey better meaning to the way parse_config works
+struct Config {
+    query: String,
+    filename: String,
+}
+
+// Moving logic for parsing the args here
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone(); // this first one will be the pattern we search for
+    let filename = args[2].clone(); // this one will be the source file we're searching into
+
+    Config { query, filename }
 }
